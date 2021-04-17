58895851beb3c225810d2efb3b701e8f61eaddef
ksviety <ksviety@gmail.com>
2021-04-18 04:00:16 +0500

    Created Camera Container Movement component

---
+++ Assets/Scripts/TowerBuilder/Gameplay/Camera/Container/Movement.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace TowerBuilder.Gameplay.Camera.Container
+{
+    [AddComponentMenu("Gameplay/Camera/Container/Movement")]
+    [DisallowMultipleComponent]
+    public class Movement : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 8f;
+        [SerializeField] private Transform _target;
+
+        private void Update()
+        {
+            var current = transform.position;
+            var destination = _target.position;
+            var factor = _speed * Time.deltaTime;
+
+            transform.position = Vector3.Lerp(current, destination, factor);
+        }
+    }
+}
