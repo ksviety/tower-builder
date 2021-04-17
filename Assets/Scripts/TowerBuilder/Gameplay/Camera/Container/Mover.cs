4b19c61eb4609ccf9f5eff3e9a42b643ab7539a8
ksviety <ksviety@gmail.com>
2021-04-18 04:55:09 +0500

    Changed camera container's mover function to LateUpdate

--- Assets/Scripts/TowerBuilder/Gameplay/Camera/Container/Mover.cs
+++ Assets/Scripts/TowerBuilder/Gameplay/Camera/Container/Mover.cs
@@ -9,7 +9,7 @@ namespace TowerBuilder.Gameplay.Camera.Container
         [SerializeField] private float _speed = 8f;
         [SerializeField] private Transform _target;
 
-        private void Update()
+        private void LateUpdate()
         {
             var current = transform.position;
             var destination = _target.position;
