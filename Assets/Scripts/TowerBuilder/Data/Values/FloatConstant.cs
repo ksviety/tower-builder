0c928c5e44f41b4a56495f743119caf31b241e67
ksviety <ksviety@gmail.com>
2021-04-18 07:07:30 +0500

    Changed float constant filename

--- Assets/Scripts/TowerBuilder/Data/Values/FloatConstant.cs
+++ Assets/Scripts/TowerBuilder/Data/Values/FloatConstant.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 namespace TowerBuilder.Data.Values
 {
-    [CreateAssetMenu(fileName = "FloatValue", menuName = "Constant/Float", order = 0)]
+    [CreateAssetMenu(fileName = "Float", menuName = "Constant/Float", order = 0)]
     public class FloatConstant : Constant<float>
     {
         
