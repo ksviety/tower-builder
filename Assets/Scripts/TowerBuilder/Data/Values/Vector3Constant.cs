55bbfec8b8933ca60074fb8e85182e9737a78415
ksviety <ksviety@gmail.com>
2021-04-18 07:08:05 +0500

    Created Vector3 constant value

---
+++ Assets/Scripts/TowerBuilder/Data/Values/Vector3Constant.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace TowerBuilder.Data.Values
+{
+    [CreateAssetMenu(fileName = "Vector3", menuName = "Constant/Vector3", order = 0)]
+    public class Vector3Constant : Constant<Vector3>
+    {
+        
+    }
+}
