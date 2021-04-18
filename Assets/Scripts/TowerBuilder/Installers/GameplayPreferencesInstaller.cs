02cfe4122ae3bf6d80e66d0888df4cd2d021e7d8
ksviety <ksviety@gmail.com>
2021-04-18 07:09:21 +0500

    Created gameplay preferences installer

---
+++ Assets/Scripts/TowerBuilder/Installers/GameplayPreferencesInstaller.cs
@@ -0,0 +1,26 @@
+using TowerBuilder.Data.Preferences;
+using UnityEngine;
+using Zenject;
+using GameplayPreferences = TowerBuilder.Data.Preferences.Gameplay;
+
+namespace TowerBuilder.Installers
+{
+    [AddComponentMenu("Installers/Gameplay Preferences Installer")]
+    public class GameplayPreferencesInstaller : MonoInstaller
+    {
+        [SerializeField] private GameplayPreferences _preferences;
+        
+        public override void InstallBindings()
+        {
+            Container
+                .Bind<IGameplayPreferences>()
+                .FromInstance(_preferences)
+                .AsCached();
+
+            Container
+                .Bind<ICameraContainerPreferences>()
+                .FromInstance(_preferences.CameraContainer)
+                .AsCached();
+        }
+    }
+}
